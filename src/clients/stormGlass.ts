a63ea736817ce81a0ff488eaed2174a899e1b728
bertolucci-alan <bertoluccialan23@gmail.com>
2022-08-11 17:06:43 -0300

    feat: create stormGlass normalized response

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,4 +1,9 @@
 import { AxiosStatic } from 'axios';
+import { ForecastPoint } from './types/ForecastPoint';
+import {
+  StormGlassForecastResponse,
+  StormGlassPoint,
+} from './types/StormGlassForecastResponse';
 
 export class StormGlass {
   readonly stormGlassApiParams =
@@ -7,9 +12,43 @@ export class StormGlass {
 
   constructor(protected request: AxiosStatic) {}
 
-  public async fetchPoints(lat: number, lng: number): Promise<any> {
-    return this.request.get(
+  public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
+    const response = await this.request.get<StormGlassForecastResponse>(
       `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassApiParams}&source=${this.stormGlassApiSource}`
+    );
+
+    return this.normalizeResponse(response.data);
+  }
+
+  public normalizeResponse(
+    points: StormGlassForecastResponse
+  ): ForecastPoint[] {
+    //fazendo o bind porque this.isValidPoint é um método da instância da classe
+    //StormGlass, caso não haja o bind, o método vai ser undefined
+    return points.hours.filter(this.isValidPoint.bind(this)).map((point) => ({
+      swellDirection: point.swellDirection[this.stormGlassApiSource],
+      swellHeight: point.swellHeight[this.stormGlassApiSource],
+      swellPeriod: point.swellPeriod[this.stormGlassApiSource],
+      time: point.time,
+      waveDirection: point.waveDirection[this.stormGlassApiSource],
+      waveHeight: point.waveHeight[this.stormGlassApiSource],
+      windDirection: point.windDirection[this.stormGlassApiSource],
+      windSpeed: point.windSpeed[this.stormGlassApiSource],
+    }));
+  }
+
+  //Partial faz com que as propriedades possam ser undefined
+  private isValidPoint(point: Partial<StormGlassPoint>): boolean {
+    //!! faz com que o retorno seja boolean
+    return !!(
+      point.time &&
+      point.swellDirection?.[this.stormGlassApiSource] &&
+      point.swellHeight?.[this.stormGlassApiSource] &&
+      point.swellPeriod?.[this.stormGlassApiSource] &&
+      point.waveDirection?.[this.stormGlassApiSource] &&
+      point.waveHeight?.[this.stormGlassApiSource] &&
+      point.windDirection?.[this.stormGlassApiSource] &&
+      point.windSpeed?.[this.stormGlassApiSource]
     );
   }
 }
---
+++ src/clients/types/StormGlassForecastResponse.ts
@@ -0,0 +1,18 @@
+export interface StormGlassForecastResponse {
+  readonly hours: StormGlassPoint[];
+}
+
+export interface StormGlassPoint {
+  readonly time: string;
+  readonly waveHeight: StormGlassPointSource;
+  readonly waveDirection: StormGlassPointSource;
+  readonly swellDirection: StormGlassPointSource;
+  readonly swellHeight: StormGlassPointSource;
+  readonly swellPeriod: StormGlassPointSource;
+  readonly windDirection: StormGlassPointSource;
+  readonly windSpeed: StormGlassPointSource;
+}
+
+export interface StormGlassPointSource {
+  readonly [key: string]: number;
+}
